467b0e1dd297fc282be1402b5c14368555bbd5e5
bustling-maker-4835 <chopra.chet@gmail.com>
2019-05-09 00:52:07 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -17,3 +17,11 @@ end
 def last_element(arr)
   return arr.last
 end
+
+def first_element_with_array_methods(arr)
+  return arr.first
+end
+
+def last_element_with_array_methods(arr)
+  return arr.last
+end
